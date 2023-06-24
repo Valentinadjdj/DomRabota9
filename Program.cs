134b447af4bcdaba43944a4c9d46dacc92414938
Valentina Korotkova <korobok.valek@mail.ru>
2023-06-24 12:04:48 +0600

    Выполнено второе задание

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа 
 // в промежутке от N до 1. Выполнить с помощью рекурсии.
 // N = 5 -> "5, 4, 3, 2, 1"
+/*
 int Prompt(string message)
 {
   Console.Write(message);
@@ -22,3 +23,25 @@ if (n < 1)
   return;
 }
 Console.WriteLine(NaturalNumber(n, 1));
+*/
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
+// в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+
+int n = Prompt("Input N: ");
+int m = Prompt("Input M: ");
+int Prompt(string message)
+{
+  Console.Write(message);
+  int result = Convert.ToInt32(Console.ReadLine());
+  return result;
+}
+
+int SumOfElements(int n, int m)
+{
+  if (n == m) return n;
+  else return SumOfElements(n + 1, m) + n;
+}
+
+Console.WriteLine(SumOfElements(n, m));
