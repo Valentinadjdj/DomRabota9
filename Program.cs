1ed618eb7d99047537f6e009ebff4c08d7b40704
Valentina Korotkova <korobok.valek@mail.ru>
2023-06-24 12:11:57 +0600

    Выполнено третье задание

--- Program.cs
+++ Program.cs
@@ -28,7 +28,7 @@ Console.WriteLine(NaturalNumber(n, 1));
 // в промежутке от M до N.
 
 // M = 1; N = 15 -> 120
-
+/*
 int n = Prompt("Input N: ");
 int m = Prompt("Input M: ");
 int Prompt(string message)
@@ -45,3 +45,27 @@ int SumOfElements(int n, int m)
 }
 
 Console.WriteLine(SumOfElements(n, m));
+*/
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int Prompt(string message)
+{
+  Console.Write(message);
+  int result = Convert.ToInt32(Console.ReadLine());
+  return result;
+}
+
+int Akkerman(int m, int n)
+{
+  if (m == 0) return n + 1;
+  if (m > 0 && n == 0) return Akkerman(m - 1, 1);
+  else return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+
+int m = Prompt("Input M: ");
+int n = Prompt("Input N: ");
+
+Console.WriteLine($"A({m},{n}) = {Akkerman(m, n)}");
